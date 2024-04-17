ccde8be2f39231c311eea9fb6e74996336fb8a08
Fun117 <fun117.kun@gmail.com>
2024-04-17 18:58:34 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      description="Description will go into a meta tag in <head />">
+      description={`${siteConfig.themeConfig.description}`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
